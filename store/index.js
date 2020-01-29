79c16cb7e4f7b8a06564414e8c29537651c38c07
tontsa74 <toni.silventoinen@tuni.fi>
2020-01-29 14:42:13 +0200

    add redux-persist

--- store/index.js
+++ store/index.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
 import allReducers from './reducers';
 import thunk from 'redux-thunk';
+import { AsyncStorage } from 'react-native';
+import { persistStore, persistReducer } from 'redux-persist';
 
 const middleware = applyMiddleware(thunk);
 
-const store = createStore(allReducers, middleware);
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['search'],
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
+const store = createStore(persistedReducer, middleware);
+
+const persistor = persistStore(store);
 
 store.subscribe(() => {
     console.log('store changed: ', store.getState());
 });
 
-export { store };
+export { store, persistor };
