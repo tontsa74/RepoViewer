35b3eb518d97fad783c4965ec508d6d1eb158fd1
tontsa74 <toni.silventoinen@tuni.fi>
2020-01-29 14:19:09 +0200

    add middleware redux-thunk

--- store/index.js
+++ store/index.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import allReducers from './reducers';
+import thunk from 'redux-thunk';
 
-const store = createStore(allReducers);
+const middleware = applyMiddleware(thunk);
+
+const store = createStore(allReducers, middleware);
 
 store.subscribe(() => {
     console.log('store changed: ', store.getState());
