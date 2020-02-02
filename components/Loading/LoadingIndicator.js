ea4adc97180db5f5d508d9084218312fd29b023c
tontsa74 <toni.silventoinen@tuni.fi>
2020-02-02 19:13:23 +0200

    add comments

--- components/Loading/LoadingIndicator.js
+++ components/Loading/LoadingIndicator.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import LoadingIndicatorStyles from './LoadingIndicator.styles';
 
+/**
+ * Loading indicator component
+ *
+ */
 export default function LoadingIndicator() {
     return (
         <View style={LoadingIndicatorStyles.container}>
--- screens/Welcome/Welcome.js
+++ screens/Welcome/Welcome.js
@@ -3,6 +3,13 @@ import { View, Text, Image } from 'react-native';
 import WelcomeStyles from './Welcome.styles';
 import LoadingIndicator from '../../components/Loading/LoadingIndicator';
 
+/**
+ * Welcome screen component
+ *
+ * Contains app logo and loading indicator
+ *
+ * @param {boolean} props loading
+ */
 export default function Welcome(props) {
     return (
         <View style={WelcomeStyles.container}>
